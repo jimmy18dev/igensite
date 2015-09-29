95b9592deebb9d9ff4dfc27e2c3b04df57b1229c
Puwadon Sricharoen <mrjimmy18@gmail.com>
2015-09-30 02:24:52 +0700

    Development 0.1
    - Create Project

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en-US" itemscope itemtype="http://schema.org/Blog" prefix="og: http://ogp.me/ns#">
+<head>
+
+<!--[if lt IE 9]>
+<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
+<![endif]-->
+
+<!-- Meta Tag -->
+<meta charset="utf-8">
+
+<!-- Viewport (Responsive) -->
+<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="user-scalable=no">
+<meta name="viewport" content="initial-scale=1,maximum-scale=1">
+<?php include'favicon.php';?>
+
+<title>Puwadon Sricharoen in IGENSITE</title>
+
+<!-- Meta Tag Main -->
+<meta name="description"          content="Web Developer and Web Designer (UI &amp; UX Design) at IGensite"/>
+
+<meta property="og:title"         content="Puwadon Sricharoen in IGENSITE"/>
+<meta property="og:description"   content="Web Developer and Web Designer (UI &amp; UX Design) at IGensite"/>
+<meta property="og:url"           content="http://igensite.com"/>
+<meta property="og:image"         content="bg.jpg"/>
+
+<meta property="og:type"          content="website"/>
+<meta property="og:site_name"     content="igensite"/>
+<meta property="fb:app_id"        content="521457164555986"/>
+<meta property="fb:admins"        content="1818320188"/>
+
+<meta name="author" content="igensite.com">
+<meta name="generator" content="IGENSITE Ver 1.0"/>
+
+<!-- Add the following three tags inside head. -->
+<meta itemprop="name" content="Puwadon Sricharoen in IGENSITE">
+<meta itemprop="description" content="Web Developer and Web Designer (UI &amp; UX Design) at IGensite">
+<meta itemprop="image" content="bg.jpg">
+
+<!-- CSS -->
+<link rel="stylesheet" href="css/reset.css" type="text/css"/>
+<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+</head>
+<body>
+<div class="container">
+	<div class="avatar">
+		<img src="https://graph.facebook.com/1818320188/picture?type=large" alt="<?php echo $me->fname;?>">
+	</div>
+	<h1 class="title">Puwadon Sricharoen</h1>
+	<p>Web Developer and Web Designer (UI &amp; UX Design) at IGensite</p>
+
+	<p>Bachelor of Science (Information Technology) King Mongkut's University of Technology North Bangkok</p>
+	<p class="contact">
+		<a href="https://www.behance.net/puwadonsricharoen" target="_blank">Behance</a> | 
+		<a href="https://www.linkedin.com/in/jimmy18dev" target="_blank">Linkedin</a> | 
+		<a href="https://www.facebook.com/puwadonsricharoen" target="_blank">Facebook</a> | 
+		<a href="https://twitter.com/PuwadonSricha" target="_blank">Twitter</a> | 
+		<a href="https://www.flickr.com/photos/jimmy18dev/" target="_blank">Flickr</a>
+	</p>
+
+	<p class="work">
+		Work : 
+		<a href="https://www.facebook.com/igensite" target="_blank">Update!</a> , 
+		<a href="http://igenvid.igensite.com" target="_blank">IGenvid</a> , 
+		<a href="https://www.tumblr.com/theme/39051" target="_blank">AGoodYear</a>
+	</p>
+</div>
+</body>
+</html>
