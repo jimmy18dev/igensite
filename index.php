cc247e9388790d0e23bc5a0545eaba5f861cd084
Puwadon Sricharoen <mrjimmy18@gmail.com>
2016-03-01 10:55:31 +0700

    Version 1.0 (development 0.0.5)

--- index.php
+++ index.php
@@ -41,33 +41,47 @@
 <!-- CSS -->
 <link rel="stylesheet" href="css/reset.css" type="text/css"/>
 <link rel="stylesheet" href="css/style.css" type="text/css"/>
+<link href='https://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>
 
 </head>
 <body>
-<div class="container">
+<div class="author">
 	<div class="avatar">
 		<img src="https://graph.facebook.com/1818320188/picture?type=square" alt="Puwadon Sricharoen">
 	</div>
-	<h1 class="title">Puwadon Sricharoen</h1>
-	<h2>Web Developer and Web Designer (UI &amp; UX Design) at IGensite.</h2>
+	<div class="name">
+		<h1>Puwadon Sricharoen</h1>
+		<p>Web Developer and Web Designer</p>	
+	</div>
 
-	<p>I'm a UI UX Designer  with over <?php echo date("Y") - 2012;?> Years experience working, Freelancing 24/7 and always passionate about new projects.</p>
-	<p>Bachelor of Science (Information Technology) King Mongkut's University of Technology North Bangkok.</p>
-	<p>"Dreams don't work Unless You do."</p>
-	<p class="contact">
-		<a href="https://www.behance.net/puwadonsricharoen" target="_blank">Behance</a> | 
-		<a href="https://www.linkedin.com/in/jimmy18dev" target="_blank">Linkedin</a> | 
-		<a href="https://www.facebook.com/puwadonsricharoen" target="_blank">Facebook</a> | 
-		<a href="https://twitter.com/PuwadonSricha" target="_blank">Twitter</a> | 
-		<a href="https://www.flickr.com/photos/jimmy18dev/" target="_blank">Flickr</a>
-	</p>
+	<div class="about">
+		<p class="qoute">"Dreams don't work Unless You do."</p>
+		<br>
+		<p>I'm a UI UX Designer  with over <strong><?php echo date("Y") - 2012;?> Years</strong> experience working, Freelancing 24/7 and always passionate about new projects.</p>
+		<p>Bachelor of Science (Information Technology) King Mongkut's University of Technology North Bangkok.</p>
+		<br>
+		<p class="contact">
+			Follow Me: 
+			<a href="https://www.behance.net/puwadonsricharoen" target="_blank">Behance</a> | 
+			<a href="https://www.linkedin.com/in/jimmy18dev" target="_blank">Linkedin</a> | 
+			<a href="https://www.facebook.com/puwadonsricharoen" target="_blank">Facebook</a> | 
+			<a href="https://twitter.com/PuwadonSricha" target="_blank">Twitter</a> | 
+			<a href="https://www.flickr.com/photos/jimmy18dev/" target="_blank">Flickr</a>
+		</p>
+		<br>
+		<p class="work">
+			Works: 
+			<a href="http://igenvid.igensite.com" target="_blank">iGenvid</a> , 
+			<a href="https://www.tumblr.com/theme/39051" target="_blank">AGoodYear</a> , 
+			<a href="https://www.facebook.com/igensite" target="_blank">Work in progress!</a>
+		</p>
+	</div>
+</div>
 
-	<p class="work">
-		Work : 
-		<a href="https://www.facebook.com/igensite" target="_blank">Update!</a> , 
-		<a href="http://igenvid.igensite.com" target="_blank">IGenvid</a> , 
-		<a href="https://www.tumblr.com/theme/39051" target="_blank">AGoodYear</a>
-	</p>
+<div class="gallery">
+	<?php for($i=1;$i<=14;$i++){?>
+	<figure class="photo-items"><img src="image/work/work_<?php echo $i;?>.jpg" alt="Puwadon Sricharoen,Web Developer,Web Designer,UI,UX Design"></figure>
+	<?php }?>
 </div>
 </body>
 </html>
